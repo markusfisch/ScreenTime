9111d9e72a1ee8d7cb75ea937de3c4703b47b1e8
Markus Fisch <mf@markusfisch.de>
2021-12-11 14:10:26 +0100

    Show < 1m instead of seconds
    The notification is updated only once per minute so showing
    seconds is irritating.

--- app/src/main/kotlin/de/markusfisch/android/screentime/data/Summary.kt
+++ app/src/main/kotlin/de/markusfisch/android/screentime/data/Summary.kt
@@ -10,7 +10,9 @@ data class Summary(
 ) {
 	fun currently(now: Long) = total + max(0, now - start)
 	fun currentlyInSeconds(now: Long) = currently(now) / 1000L
-	fun currentlyColloquial(now: Long) = timeColloquial(currentlyInSeconds(now))
+	fun currentlyColloquial(now: Long) = timeRangeColloquial(
+		currentlyInSeconds(now)
+	)
 }
 
 fun summarizeDay(timestamp: Long = System.currentTimeMillis()): Summary {
@@ -31,8 +33,8 @@ fun summarizeDay(timestamp: Long = System.currentTimeMillis()): Summary {
 	)
 }
 
-fun timeColloquial(seconds: Long): String = when (seconds) {
-	in 0..59 -> String.format("%ds", seconds)
+private fun timeRangeColloquial(seconds: Long): String = when (seconds) {
+	in 0..59 -> "< 1m"
 	in 60..3599 -> String.format("%dm", (seconds / 60) % 60)
 	in 3600..3660 -> "1h"
 	else -> String.format(
