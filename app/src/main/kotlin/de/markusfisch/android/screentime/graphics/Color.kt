4a3b81bb29e4d6c86a2d25f2aec2242f9568d3d4
Markus Fisch <mf@markusfisch.de>
2022-06-17 14:09:20 +0200

    Fix indent

--- app/src/main/kotlin/de/markusfisch/android/screentime/graphics/Color.kt
+++ app/src/main/kotlin/de/markusfisch/android/screentime/graphics/Color.kt
@@ -6,8 +6,8 @@ import android.os.Build
 fun Context.loadColor(resId: Int): Int = if (
 	Build.VERSION.SDK_INT < Build.VERSION_CODES.M
 ) {
-    @Suppress("DEPRECATION")
-    resources.getColor(resId)
+	@Suppress("DEPRECATION")
+	resources.getColor(resId)
 } else {
 	resources.getColor(resId, theme)
 }
