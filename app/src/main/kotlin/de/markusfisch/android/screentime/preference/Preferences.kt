786d2d44a5a417bcbf7c558a57a93327e6034c6f
Markus Fisch <mf@markusfisch.de>
2024-04-20 21:22:01 +0200

    Move minDurationLengthenChoice next to preferences
    To group similar field declarations for clear, predictable
    structuring.

--- app/src/main/kotlin/de/markusfisch/android/screentime/preference/Preferences.kt
+++ app/src/main/kotlin/de/markusfisch/android/screentime/preference/Preferences.kt
@@ -12,6 +12,12 @@ import kotlin.math.min
 class Preferences(context: Context) {
 	private val preferences: SharedPreferences =
 		PreferenceManager.getDefaultSharedPreferences(context)
+	private val minDurationLengthenChoice = intArrayOf(
+		0, 1000, 5000, 10000, 15000, 20000, 30000, 40000,
+		50000, 60000, 80000, 100000, 120000, 150000, 3 * 60000, 4 * 60000,
+		5 * 60000, 6 * 60000, 7 * 60000, 8 * 60000, 9 * 60000, 10 * 60000, 12 * 60000, 15 * 60000,
+		20 * 60000, 30 * 60000, 40 * 60000, 50 * 60000, 60 * 60000
+	)
 
 	var graphRange = 0
 		set(value) {
@@ -24,13 +30,6 @@ class Preferences(context: Context) {
 			apply(HOUR_OF_DAY_CHANGE, clamped)
 			field = clamped
 		}
-
-	private val minDurationLengthenChoice = intArrayOf(
-		0, 1000, 5000, 10000, 15000, 20000, 30000, 40000,
-		50000, 60000, 80000, 100000, 120000, 150000, 3 * 60000, 4 * 60000,
-		5 * 60000, 6 * 60000, 7 * 60000, 8 * 60000, 9 * 60000, 10 * 60000, 12 * 60000, 15 * 60000,
-		20 * 60000, 30 * 60000, 40 * 60000, 50 * 60000, 60 * 60000
-	)
 	var minDurationLengthen = 0
 		set(value) {
 			apply(MIN_DURATION_LENGTHEN, value)
